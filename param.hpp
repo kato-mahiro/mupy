b8140ef05d356ccb2b62f23b580be219bf635646
katomasahiro10 <katomasahiro10@gmail.com>
2020-02-04 00:51:28 +0900

    create param.hpp
    可変のパラメータの幅を定義するファイル

---
+++ param.hpp
@@ -0,0 +1,13 @@
+#ifndef param_hpp
+#define param_hpp
+
+const double bias_upper_limit = 10.0;
+const double bias_lower_limit = -10.0;
+
+const double weight_upper_limit = 1.0;
+const double weight_lower_limit = -1.0;
+
+const double evoparam_upper_limit = 1.0;
+const double evoparam_lower_limit = -1.0;
+
+#endif
