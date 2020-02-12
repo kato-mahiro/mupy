d610f24259d273bf15a942e4bcd3eb84f7e622ea
katomasahiro10 <katomasahiro10@gmail.com>
2020-02-12 22:41:29 +0900

    add global parameters

--- param.hpp
+++ param.hpp
@@ -1,6 +1,19 @@
 #ifndef param_hpp
 #define param_hpp
 
+const int INPUT_NUM = 10;
+const int OUTPUT_NUM = 10;
+
+const int NORMAL_NUM_LOWER_LIMIT = 1;
+const int NORMAL_NUM_UPPER_LIMIT = 10;
+const int MODULATION_NUM_LOWER_LIMIT = 1;
+const int MODULATION_NUM_UPPER_LIMIT = 10;
+
+const int NEURON_NUM_UPPER_LIMIT = 20;
+
+const int CONNECTION_NUM_LOWER_LIMIT = 10;
+const int CONNECTION_NUM_UPPER_LIMIT = 100;
+
 const double BIAS_UPPER_LIMIT = 10.0;
 const double BIAS_LOWER_LIMIT = -10.0;
 
@@ -9,5 +22,8 @@ const double WEIGHT_LOWER_LIMIT = -1.0;
 
 const double EVOPARAM_UPPER_LIMIT = 1.0;
 const double EVOPARAM_LOWER_LIMIT = -1.0;
+
+const double EPSIRON_LOWER_LIMIT = 0.01;
+const double EPSIRON_UPPER_LIMIT = 10.0
 
 #endif
